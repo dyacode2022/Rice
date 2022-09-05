eb80be5b590a36ca98bb79c76484bb04a101b9e9
dya-code <entrydia7691@gmail.com>
2022-09-05 21:00:59 +0900

    config cors

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    proxy: {
-      '/rice': {
-        target: 'https://rice-backend.herokuapp.com',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/info': {
-        target: 'https://rice-backend.herokuapp.com',
-        changeOrigin: true,
-        secure: false,
-      },
-      cors:false
-    },
-  },
+  devServer: {
+    proxy : 'https://rice-backend.herokuapp.com/'
+  }
 })
